a86f0a7c4345e500fdadfc33bcae8faee33bc2b5
Babi Mumba <babimumba243@gmail.com>
2022-09-11 22:55:48 +0200

    first commit

---
+++ app/src/main/java/com/bstonetech/achatapp/AchatActivity.java
@@ -0,0 +1,14 @@
+package com.bstonetech.achatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AchatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achat);
+    }
+}
