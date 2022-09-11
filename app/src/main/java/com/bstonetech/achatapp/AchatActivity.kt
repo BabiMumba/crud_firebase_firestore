fa5eb88c1a80d2f81dd623c9ccbd670d20b0a23a
Babi Mumba <babimumba243@gmail.com>
2022-09-11 22:57:03 +0200

    convert a java class to kotlin

---
+++ app/src/main/java/com/bstonetech/achatapp/AchatActivity.kt
@@ -0,0 +1,12 @@
+package com.bstonetech.achatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bstonetech.achatapp.R
+
+class AchatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_achat)
+    }
+}
