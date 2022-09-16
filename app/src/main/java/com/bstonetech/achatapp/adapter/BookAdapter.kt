d7dfc02911522f435f3e9345aab472ce69515611
Babi Mumba <babimumba243@gmail.com>
2022-09-16 15:44:38 +0200

    add new file

--- app/src/main/java/com/bstonetech/achatapp/adapter/BookAdapter.kt
+++ app/src/main/java/com/bstonetech/achatapp/adapter/BookAdapter.kt
@@ -1,6 +1,7 @@
 package com.bstonetech.achatapp.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -10,20 +11,21 @@ import com.bstonetech.achatapp.R
 import com.bstonetech.achatapp.model.BookModel
 import org.w3c.dom.Text
 
-class BookAdapter(var context: Context, var arrayList: ArrayList<BookModel>):  RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+class BookAdapter(var context: Context, var arrayList: ArrayList<BookModel>):  RecyclerView.Adapter<BookAdapter.itemHolder>(){
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): itemHolder{
+        val itemHolder = LayoutInflater.from(parent.context).inflate(R.layout.row_grid_view,parent,false)
+        return itemHolder(itemHolder)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: itemHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
     override fun getItemCount(): Int {
         return arrayList.size
     }
-    class itemview(itemview: View):RecyclerView.ViewHolder(itemview){
+    class itemHolder(itemview: View):RecyclerView.ViewHolder(itemview){
         var icone:ImageView = itemview.findViewById(R.id.icons_image)
         var title:TextView = itemview.findViewById(R.id.title)
 
