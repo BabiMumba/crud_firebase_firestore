adb18767104013c97913115fbf11f5cb47082808
Babi Mumba <babimumba243@gmail.com>
2022-09-16 15:35:15 +0200

    add new file

--- app/src/main/java/com/bstonetech/achatapp/model/BookModel.kt
+++ app/src/main/java/com/bstonetech/achatapp/model/BookModel.kt
@@ -1,4 +1,11 @@
 package com.bstonetech.achatapp.model
 
 class BookModel {
+    var icon:Int ? = 0
+    var tittre: String ?= null
+
+    constructor(icon: Int?, tittre: String?) {
+        this.icon = icon
+        this.tittre = tittre
+    }g
 }
