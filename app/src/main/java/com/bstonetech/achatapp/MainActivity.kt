e6d1bade29f863723b1c096cf639d8756f52e583
Babi Mumba <babimumba243@gmail.com>
2022-09-22 23:14:43 +0200

    add new file

--- app/src/main/java/com/bstonetech/achatapp/MainActivity.kt
+++ app/src/main/java/com/bstonetech/achatapp/MainActivity.kt
@@ -2,17 +2,42 @@ package com.bstonetech.achatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
         replaceFragment(HomeFragment())
     }
-    private fun replaceFragment(homeFragment: HomeFragment){
+    private fun replaceFragment(fragment: Fragment){
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.my_frame,homeFragment)
+        fragmentTransaction.replace(R.id.my_frame,fragment)
         fragmentTransaction.commit()
     }
+
+    private val mOnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            val fragment: Fragment
+            when (item.itemId) {
+                R.id.One -> {
+                    fragment = HomeFragment()
+                    replaceFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.Two -> {
+                    fragment = FragmentTwo()
+                    replaceFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+
+            }
+            false
+        }
 }
