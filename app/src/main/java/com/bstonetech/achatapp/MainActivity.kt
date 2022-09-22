6c5d7f7d7badba90307d087a543f78df73b6d1ca
Babi Mumba <babimumba243@gmail.com>
2022-09-22 21:36:45 +0200

    add new file

--- app/src/main/java/com/bstonetech/achatapp/MainActivity.kt
+++ app/src/main/java/com/bstonetech/achatapp/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        replaceFragment(HomeFragment())
     }
     private fun replaceFragment(homeFragment: HomeFragment){
         val fragmentManager = supportFragmentManager
