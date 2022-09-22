1ff922d27c6f93180d5cc510f532414e8b19a358
Babi Mumba <babimumba243@gmail.com>
2022-09-22 21:23:32 +0200

    new file add new f

--- app/src/main/java/com/bstonetech/achatapp/HomeFragment.kt
+++ app/src/main/java/com/bstonetech/achatapp/HomeFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bstonetech.achatapp.adapter.NewAdapter
+import com.bstonetech.achatapp.model.News
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+
 
 /**
  * A simple [Fragment] subclass.
@@ -20,6 +25,16 @@ class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var adapter: NewAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var newsarrayList: ArrayList<News>
+
+    lateinit var imageId:Array<Int>
+    lateinit var heading:Array<String>
+    lateinit var news:Array<String>
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,5 +70,33 @@ class HomeFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+    private fun datainitialize(){
+        newsarrayList = arrayListOf<News>()
+
+        imageId = arrayOf(
+            R.drawable.a,
+            R.drawable.b,
+            R.drawable.c,
+            R.drawable.d,
+            R.drawable.e,
+            R.drawable.f,
+            R.drawable.g,
+            R.drawable.h,
+            R.drawable.i,
+            R.drawable.j,
+        )
+        heading = arrayOf(
+            getString(R.string.head_1),
+            getString(R.string.head_2),
+            getString(R.string.head_3),
+            getString(R.string.head_4),
+            getString(R.string.head_5),
+            getString(R.string.head_6),
+            getString(R.string.head_7),
+            getString(R.string.head_8),
+            getString(R.string.head_9),
+            getString(R.string.head_10)
+        )
     }
 }
