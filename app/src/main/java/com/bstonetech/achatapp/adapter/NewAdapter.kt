309c9615b4aaac4f08368e7d5185f5feedccf587
Babi Mumba <babimumba243@gmail.com>
2022-09-22 21:02:24 +0200

    add enw file

--- app/src/main/java/com/bstonetech/achatapp/adapter/NewAdapter.kt
+++ app/src/main/java/com/bstonetech/achatapp/adapter/NewAdapter.kt
@@ -1,10 +1,25 @@
 package com.bstonetech.achatapp.adapter
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bstonetech.achatapp.model.News
 
 class NewAdapter(private val newslist: ArrayList<News>):RecyclerView.Adapter<NewAdapter.MyviewHolder> {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyviewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyviewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 
     class MyviewHolder(itemview:View):RecyclerView.ViewHolder(itemview){
 
