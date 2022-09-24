7554b78b2fcebe088a2554a91ab6c4c8ea95d3b1
Babi Mumba <babimumba243@gmail.com>
2022-09-24 11:44:30 +0200

    add new file

--- app/src/main/java/com/bstonetech/achatapp/NewsActivity.kt
+++ app/src/main/java/com/bstonetech/achatapp/NewsActivity.kt
@@ -2,11 +2,30 @@ package com.bstonetech.achatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        val heading_news:TextView = findViewById(R.id.titre_news)
+        val image_news:ImageView = findViewById(R.id.image_id)
+        val news:TextView = findViewById(R.id.news)
+
+        val bundle:Bundle? = intent.extras
+
+        val heading = bundle!!.getString("titre")
+        val imageId = bundle.getInt("imageId")
+        val news_tv = bundle.getString("news")
+
+        heading_news.text = heading
+        news.text = news_tv
+        image_news.setImageResource(imageId)
+
+
+
 
     }
 }
