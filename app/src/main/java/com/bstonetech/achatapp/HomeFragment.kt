c86a4fea949fe2039539c306675c3fa7d4dff6e6
Babi Mumba <babimumba243@gmail.com>
2022-09-24 12:24:42 +0200

    dd new file

--- app/src/main/java/com/bstonetech/achatapp/HomeFragment.kt
+++ app/src/main/java/com/bstonetech/achatapp/HomeFragment.kt
@@ -11,22 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bstonetech.achatapp.adapter.NewAdapter
 import com.bstonetech.achatapp.model.News
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private lateinit var adapter: NewAdapter
     private lateinit var recyclerView: RecyclerView
@@ -40,10 +25,7 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -52,27 +34,6 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -86,7 +47,10 @@ class HomeFragment : Fragment() {
         recyclerView.setHasFixedSize(true)
         adapter = NewAdapter(newsarrayList,context )
         recyclerView.adapter = adapter
-        adapter.setOnclickListener(object:NewAdapter.onItemClickListene{
+
+        /*
+          adapter.setOnclickListener(object:NewAdapter.onItemClickListene{
+
             override fun onItemClick(position: Int) {
 
                 val intent = Intent(context,NewsActivity::class.java)
@@ -98,6 +62,8 @@ class HomeFragment : Fragment() {
             }
 
         })
+         */
+
     }
     private fun datainitialize(){
         newsarrayList = arrayListOf<News>()
