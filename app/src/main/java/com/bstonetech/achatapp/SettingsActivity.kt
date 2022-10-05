cabdb425d3a8d668cf8da41597ef8c8f35120391
Babi Mumba <babimumba243@gmail.com>
2022-10-05 15:28:28 +0200

    add new file

--- app/src/main/java/com/bstonetech/achatapp/SettingsActivity.kt
+++ app/src/main/java/com/bstonetech/achatapp/SettingsActivity.kt
@@ -14,6 +14,7 @@ import com.bstonetech.achatapp.mode_theme.DarkModePref
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         if (DarkModePref(this).isNightMode) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         }
